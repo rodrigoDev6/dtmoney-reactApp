c74c332f5477439016ad5c39ffb396d110d0861d
rodrigodev6 <rodrigolima.alves21@gmail.com>
2022-07-29 09:12:25 -0300

    using miragejs, para consumir dados ficticios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
 import { Header } from './components/Header'
 import { GlobalStyle } from './styles/global'
 import { Dashboard } from './components/Dashboard'
+import { createServer } from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Sistema PDV',
+          value: -1000,
+          type: 'withdraw',
+          category: 'Compra',
+        },
+      ]
+    })
+  },
+})
+
 export function App() {
   return (
     <>
